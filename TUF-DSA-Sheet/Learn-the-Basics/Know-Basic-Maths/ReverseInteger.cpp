7287d230d2c534b2cdab26ee693013173abfcad9
CODSensei <krishna2342005@gmail.com>
2024-08-18 17:22:11 +0530

    did some mathematical problems

--- TUF-DSA-Sheet/Learn-the-Basics/Know-Basic-Maths/ReverseInteger.cpp
+++ TUF-DSA-Sheet/Learn-the-Basics/Know-Basic-Maths/ReverseInteger.cpp
@@ -4,21 +4,15 @@ using namespace std;
 int main() {
 	
 	int n;
-	int x;
-    n=x;
+	cin	>> n;
 	int reverse = 0;
-    if(n<0) return false;
+	if (n>INT_MAX || n<INT_MIN) return 0;
 	while(n){
 		int last=n%10;
 		n/=10;
-		if (reverse>INT_MAX/10 || reverse<INT_MIN/10) return false;
 		reverse = (reverse*10) + last;
 	}
 	cout << reverse;
-	if(x == reverse)
-		return true;
-	else return false;
-    
 	return 0;
 }
 
