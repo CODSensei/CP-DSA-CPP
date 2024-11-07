c6def543824640519ddd9b1a17941d2cda79e1e6
CODSensei <krishna2342005@gmail.com>
2024-11-07 14:33:57 +0530

    started neetcode.io also

--- leetCode/TwoSum1.cpp
+++ leetCode/TwoSum1.cpp
@@ -1,26 +1,13 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices;
-        int index = 0;
-        for(auto it=nums.begin(); it!=nums.end(); it++){
-            int indexPlus = index +1;
-            int compliment = target - *(it);
-            for(auto i = it+1; i!=nums.end(); i++){
-                if(*(i) == compliment) {
-                    indices.push_back(index);
-                    indices.push_back(indexPlus);
-                    return indices;
-                }
-                else {
-                    indexPlus++;
-                }
-            }
-            index++;
-        }
-        return {};
-    }
-};
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+//Type defined
+typedef vector<long long> vll;
+typedef vector<int> vi;
+
+//Macros
+#define pb push_back
 
 //My Solution
 class Solution {
