418d429c0341d50f1b7f720322c6df0c31fddf50
CODSensei <krishna2342005@gmail.com>
2025-08-18 22:15:56 +0530

    852. Peak Index in a Mountain Array - leetcode v2

--- coders-army/L_24/peakIndexInAMountainArray.cpp
+++ coders-army/L_24/peakIndexInAMountainArray.cpp
@@ -10,6 +10,7 @@ typedef vector<char> vc;
 //Macros
 #define pb push_back
 
+// 1st method
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
@@ -22,6 +23,24 @@ public:
                 start = mid + 1;
             else
                 end = mid - 1;
+        }
+        return 0;
+    }
+};
+
+// 2nd method
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 0, end = arr.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                return mid;
+            else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1])
+                start = mid + 1;
+            else
+                end = mid;
         }
         return 0;
     }
