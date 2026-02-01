3b392c96c2df75adca6886aad28c65613c771a4d
CODSensei <krishna2342005@gmail.com>
2026-02-01 22:01:28 +0530

    fresh start from 1 feb 2026

--- README.md
+++
@@ -1,6 +0,0 @@
-This file is for people who are solving DSA and CP problems on popular sites like LeetCode, CodeForces, Coder's Army etc and want to know solutions of problems given. 
-
-This file will contain solution of Problems attempted by me.
-It also contains my sublime text build.
-
-PS: Solution given here might be crappy as I am also a beginner
--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include<stdio.h>
 using namespace std;
 
+// ios::sync_with_stdio(0);
+// cin.tie(0);
+
 //Type defined
 typedef vector<long long> vll;
 typedef vector<int> vi;
@@ -10,37 +13,9 @@ typedef vector<char> vc;
 //Macros
 #define pb push_back
 
-class Solution {
-public:
-    bool isPrime(int x) {
-        if (x == 1)
-            return false;
-        if (x == 2)
-            return true;
-        if (x % 2 == 0)
-            return false;
-        for (long long i = 3; i * i <= x; i += 2) {
-            if (x % i == 0)
-                return false;
-        }
-        return true;
-    }
-    bool completePrime(int num) {
-        if (num < 10 && isPrime(num))
-            return true;
-        string s = to_string(num);
-        if (isPrime(num)) {
-            for (int i = 1; i <= s.size(); i++) {
-                if (!isPrime(stoi(s.substr(0, i)))) {
-                    return false;
-                }
-            }
-            for (int i = 0; i < s.size(); i++) {
-                if (!isPrime(stoi(s.substr(i))))
-                    return false;
-            }
-            return true;
-        }
-        return false;
-    }
-};
+int main() {
+    int n;
+    cin >> n;
+    cout << n;
+    return 0;
+}
